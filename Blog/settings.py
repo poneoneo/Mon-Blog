7c3fee568a4c478188ad05a5a1700c1c8c9daf21
poneoneo <onealzero@gmail.com>
2023-04-18 11:22:31 +0100

    Reconstitution de la page index.html a l'identique
    -probleme de chargement de fichier statiques resolu.(il falait copier le dossier "assets" dans le dossier "static")
    -renomage les urls(href) des fichiers statiques pour rendu sur la page index.html.

--- Blog/settings.py
+++ Blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "articles",
+
     "user_cust",
 ]
 
@@ -119,8 +120,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILESDIRS = [
-    BASE_DIR / "static",
+
+STATICFILES_DIRS = [ 
+    BASE_DIR / "static/",
 ]
 
 # Media files upload (video, Images,docx)
